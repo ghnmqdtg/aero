df5b65b23f1bd08712be656f322e20b68c5f8b2e
ghnmqdtg <ghnmqdtg@gmail.com>
2024-04-23 14:48:51 +0800

    Feat: Add yaml files for training 2kHz to 16kHz

---
+++ slurm_train.sh
@@ -0,0 +1,9 @@
+#! /bin/bash
+
+#Batch Job Paremeters
+#SBATCH --nodes=1
+#SBATCH --gpus-per-node=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --account=MST112230
+
+python train.py dset=2-16 experiment=aero_2-16_512_64
